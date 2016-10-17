c4ffc39f58d858f043bf33bae0ade32eb01199d8
Musee Ullah <musee.ullah@fireeye.com>
2016-10-17 15:52:17 -0700

    Use an LXC container instead of Docker for testing

--- tests/launch-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-cuda:rw local/c7-systemd /bin/bash -c "/ansible-role-cuda/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
