623d3d638ec522bb9965e9f4159ce0f2129cc01c
Musee Ullah <musee.ullah@fireeye.com>
2016-10-17 20:07:17 -0700

    Better handle updating user environment for CUDA

---
+++ files/cuda.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export CUDA_ROOT=/usr/local/cuda
+export CUDA_INC_DIR=/usr/local/cuda/bin:$CUDA_INC_DIR
+export CPATH=/usr/local/cuda/include:$CPATH
